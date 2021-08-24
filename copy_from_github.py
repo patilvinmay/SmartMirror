358bb28072a8ec88104f584575c3807d2ba29b9a
Vinmay Patil <patilvinmay@gmail.com>
2021-08-24 16:26:25 +0530

    Create copy_from_github.py

---
+++ copy_from_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import requests
+import os
+
+url = 'https://raw.githubusercontent.com/patilvinmay/SmartMirror/master/Hello.py'
+req = requests.get(url)
+pycode = req.text
+
+os.getcwd()
+
+f = open("temp.py", "w")
+f.write(pycode)
+f.close()
